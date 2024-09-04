8f5d74ea7237b43569fbdbee9378c544ba283431
Jamanul <jk.skb.25@gmail.com>
2024-09-04 11:52:17 +0600

    added gesture

--- src/app/animate/page.jsx
+++ src/app/animate/page.jsx
@@ -2,11 +2,11 @@
 import React, { useEffect, useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 const page = () => {
-    const [visible,setVisible]=useState(true)
+    const [visible,setVisible]=useState(false)
   return (
     <div className="flex flex-col gap-2 items-center min-h-screen justify-center ">
-        <button onClick={()=>setVisible(!visible)}>Set visible</button>
-      <AnimatePresence>
+        <motion.button layout onClick={()=>setVisible(!visible)}>Set visible</motion.button>
+      <AnimatePresence mode="popLayout">
        {visible &&  <motion.div
           initial={{
             rotate: "0deg",
@@ -15,6 +15,7 @@ const page = () => {
           animate={{
             rotate: "180deg",
             scale: 1,
+            y: [0,150,-150,-150,0]
           }}
           exit={{
             rotate: "0deg",
@@ -22,7 +23,8 @@ const page = () => {
           }}
           transition={{
             duration :1 ,
-            ease:"easeInOut"
+            ease:"easeInOut",
+            times:[0,0.25,0.5,0.9,1]
           }}
           style={{
             height: "180px",
@@ -31,6 +33,12 @@ const page = () => {
           }}
         ></motion.div>}
       </AnimatePresence>
+        <div>
+            <motion.button whileHover={{scale:1.1}} whileTap={{scale:0.95,rotate: '2.5deg'}} transition={{duration:1}} className="bg-red-600 text-white">
+                click me
+            </motion.button>
+        </div>
+
     </div>
   );
 };
